31a8c681a427cdce5d45d78945ab8d76c518ddfc
Han <hanyu2@asu.edu>
2016-07-21 16:48:18 -0700

    Add render bin list

--- imports/collections/bins.js
+++ imports/collections/bins.js
@@ -1,14 +1,14 @@
-import React from 'react';
+import { Mongo } from 'meteor/mongo';
 
 Meteor.methods({
-	'bins.insert': function(){
-		return Bins.insert({
-			createdAt: new Date(),
-			content: '',
-			sharedWith: [],
-			ownerId: this.userId
-		});
-	}
+  'bins.insert': function() {
+    return Bins.insert({
+      createdAt: new Date(),
+      content: '',
+      sharedWith: [],
+      ownerId: this.userId
+    });
+  }
 });
 
 export const Bins = new Mongo.Collection('bins');
