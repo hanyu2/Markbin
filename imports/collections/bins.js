262b0d50351e4f27f2a613577678b480ed1e9479
Han <hanyu2@asu.edu>
2016-07-22 16:02:34 -0700

    Update meteor method

--- imports/collections/bins.js
+++ imports/collections/bins.js
@@ -10,8 +10,12 @@ Meteor.methods({
     });
   },
 
-  'bins.remove': function(bin){
-  	return Bins.remove(bin);
+  'bins.remove': function(bin) {
+    return Bins.remove(bin);
+  },
+
+  'bins.update': function(bin, content) {
+    return Bins.update(bin._id, { $set: { content } });
   }
 });
 
